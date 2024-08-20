931197d805886fae5101c41bbb0653970e83e3f2
Nithin Monni <monni016@umn.edu>
2024-08-20 05:05:07 -0500

    Blog title, header, and description

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Dashboard
+    <main className="flex min-h-screen flex-col justify-between py-20">
+      <article className="">
+        <header className="text-4xl font-extrabold">
+          My Love/Hate Relationship With Vercel
+        </header>
+        <div className="font-thin pt-2 opacity-50">
+          Posted Tuesday, Aug 20, 2024 - 200 words, ~2 minutes
+        </div>
+
+        <div></div>
+      </article>
     </main>
   );
 }
