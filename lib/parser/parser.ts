c0711ec2c97e37075608fa5bec773d1ee7772c5d
Nithin Monni <monni016@umn.edu>
2024-08-24 19:53:00 -0500

    initially grad title and date

--- lib/parser/parser.ts
+++ lib/parser/parser.ts
@@ -1,3 +1,47 @@
+  const file = await fs.readFile(`/posts/${postFile}`, "utf8");
+  const lines = file.split("\n");
+
+  const date = new Date(lines[0]);
+  const title = lines[1];
+
+  let words = 0;
+  for (const line of lines) {
+    for (const _word of line.split(" ")) words++;
+  }
+
+  const daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthsOfTheYear = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const infoString = `Posted ${daysOfTheWeek[date.getDay()]}, ${
+    monthsOfTheYear[date.getMonth()]
+  } ${date.getDate()}, ${date.getFullYear()} - ${words} words, ~${Math.ceil(
+    words / 200
+  )} minutes`;
+
+  lines.shift();
+  lines.shift();
+
   type Data = (
     | {
         type:
